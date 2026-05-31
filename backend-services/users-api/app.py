e9d80b8e9bb29323c3f0e785ccf04f09469f83d9
Avenis3010 <https://github.com/Avenis3010/ignite-devops-task.git>
2026-05-31 17:39:47 +0530

    feat: add dev/prod pipelines, ingress paths /dev /prod, ArgoCD branch tracking

--- backend-services/users-api/app.py
+++ backend-services/users-api/app.py
@@ -1,14 +1,22 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Backend API Running version2"
+ENV = os.getenv("APP_ENV", "dev")
 
-@app.route("/health")
+@app.route(f"/{ENV}/")
+@app.route(f"/{ENV}")
+def home():
+    return f"Backend API Running - Environment: {ENV}"
+
+@app.route(f"/{ENV}/health")
 def health():
     return "OK"
+
+@app.route(f"/{ENV}/users")
+def users():
+    return {"env": ENV, "users": []}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
